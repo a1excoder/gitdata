8e15e2d6ddb40fc4cb739f2b32378ecbd2aa8558
a1excoder <alexnxman@gmail.com>
2021-12-08 21:22:43 +0200

    upload GetRepos func and structs for him

--- gitdata.go
+++ gitdata.go
@@ -8,7 +8,7 @@ import (
 )
 
 func (udP *UserData) GetUserData(user_name string) (int, error) {
-	resp, err := http.Get("https://api.github.com/users/" + user_name)
+	resp, err := http.Get(fmt.Sprintf("https://api.github.com/users/%s", user_name))
 	if err != nil {
 		return resp.StatusCode, err
 	}
@@ -24,4 +24,70 @@ func (udP *UserData) GetUserData(user_name string) (int, error) {
 
 	err = json.Unmarshal(json_decode, udP)
 	return resp.StatusCode, err
+}
+
+func GetRepos(user_name string, repos_number int) (int, []MainReposData, error) {
+	repos := make([]MainReposData, 0, repos_number)
+
+	if repos_number <= 100 {
+		resp, err := http.Get(fmt.Sprintf("https://api.github.com/users/%s/repos?per_page=100&sort=updated&direction=desc&page=1", user_name))
+		if err != nil {
+			return resp.StatusCode, nil, err
+		}
+
+		if resp.StatusCode == 404 {
+			return resp.StatusCode, nil, fmt.Errorf("Not Found")
+		}
+
+		json_decode, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return resp.StatusCode, nil, err
+		}
+
+		err = json.Unmarshal(json_decode, &repos)
+		if err != nil {
+			return resp.StatusCode, nil, err
+		}
+
+		return 200, repos, nil
+	}
+
+	temp, temp2 := float32(repos_number)/100.0, repos_number
+	if temp == float32(int(temp)) {
+		temp = float32(int(temp))
+	} else {
+		temp = float32(int(temp) + 1)
+	}
+	fmt.Println(temp)
+	for i := 1; i <= int(temp); i++ {
+		fmt.Println(temp2)
+		temp_repos := make([]MainReposData, 0, temp2)
+
+		resp, err := http.Get(fmt.Sprintf("https://api.github.com/users/%s/repos?per_page=100&sort=updated&direction=desc&page=%d", user_name, i))
+		if err != nil {
+			return resp.StatusCode, nil, err
+		}
+
+		if resp.StatusCode == 404 {
+			return resp.StatusCode, nil, fmt.Errorf("Not Found")
+		}
+
+		json_decode, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return resp.StatusCode, nil, err
+		}
+
+		err = json.Unmarshal(json_decode, &temp_repos)
+		if err != nil {
+			return resp.StatusCode, nil, err
+		}
+
+		for _, rep_ := range temp_repos {
+			repos = append(repos, rep_)
+		}
+
+		temp2 -= 100
+	}
+
+	return 200, repos, nil
 }
--- json_types.go
+++ json_types.go
@@ -34,3 +34,60 @@ type UserData struct {
 	Created_at          string
 	Updated_at          string
 }
+
+type owner struct {
+	Login               string
+	Id                  int64
+	Node_id             string
+	Avatar_url          string
+	Gravatar_id         string
+	Url                 string
+	Html_url            string
+	Followers_url       string
+	Following_url       string
+	Gists_url           string
+	Starred_url         string
+	Subscriptions_url   string
+	Organizations_url   string
+	Repos_url           string
+	Events_url          string
+	Received_events_url string
+	Type                string
+	Site_admin          bool
+}
+
+type license struct {
+	Key     string
+	Name    string
+	Spdx_id string
+	Url     string
+	Node_id string
+}
+
+type MainReposData struct {
+	Id                int64
+	Node_id           string
+	Name              string
+	Full_name         string
+	Private           bool
+	Owner             owner
+	Html_url          string
+	Fork              bool
+	Url               string
+	Forks_url         string
+	Collaborators_url string
+	Commits_url       string
+	Issues_url        string
+	Stargazers_url    string
+	Created_at        string
+	Updated_at        string
+	Pushed_at         string
+	Size              int
+	Stargazers_count  int
+	Language          string
+	Forks_count       int
+	Archived          bool
+	License           license
+	Open_issues       int
+	Default_branch    string
+}
